aefb29fbb62f48a6f51a7bfc9a753de87c8e0e3f
HugoDMiranda <hdmirandab@hotmail.com>
2023-09-22 23:21:39 -0500

    home payment update

--- src/components/MediosDePago.js
+++ src/components/MediosDePago.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { GoBell } from "react-icons/go";
 import "../styles/styles-components/MediosDePago.css";
 
-function MediosDePago() {
+function MediosDePago({ paymentText, paymentIcon }) {
   return (
     <>
       <div className="payment">
-        <Link>
-          <GoBell />
-        </Link>
+        <Link className="payment-icon">{paymentIcon}</Link>
         <div>
-          <h3>Hasta 48 horas</h3>
+          <h3>{paymentText}</h3>
           <Link>Ver mas</Link>
         </div>
       </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,16 +3,31 @@ import SliderStores from "../components/SliderStores";
 import SliderPromos from "../components/SliderPromos";
 import "../styles/Home.css";
 import MediosDePago from "../components/MediosDePago";
+import { BsFillCreditCard2BackFill, BsBank } from "react-icons/bs";
+import { GiTakeMyMoney } from "react-icons/gi";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 function Home() {
   return (
     <div className="home">
       <SliderPromos />
       <div className="home-payment">
-        <MediosDePago />
-        <MediosDePago />
-        <MediosDePago />
-        <MediosDePago />
+        <MediosDePago
+          paymentText={`Hasta 48 cuotas`}
+          paymentIcon={<BsFillCreditCard2BackFill size={30} />}
+        />
+        <MediosDePago
+          paymentText={`Tranferencia desde tu banco`}
+          paymentIcon={<BsBank size={30} />}
+        />
+        <MediosDePago
+          paymentText={`Paga en efectivo`}
+          paymentIcon={<GiTakeMyMoney size={30} />}
+        />
+        <MediosDePago
+          paymentText={`Mas medios de pago`}
+          paymentIcon={<AiFillPlusCircle size={30} />}
+        />
       </div>
       <div className="home-stores">
         <h3>Las mejores tiendas te esperan</h3>
